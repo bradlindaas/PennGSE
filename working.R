b134cf1faa79cced8e7c87d8edf67a8fb1558a59
Brad Lindaas <brad@lindaas.org>
2015-08-10 19:44:20 -0500

    MT Final Update

--- working.R
+++ working.R
@@ -1,7 +1,7 @@
 
 library(relaimpo)
 
-fit <- lm(BE~Teach+Signup+Signup_CAGR+Ret+Buy_Class+Buy_Class_CAGR+Class_Price+Course_Cost+Users_Per_Mod+Users_Per_Mod_CAGR+Mod_Cost+Seat_Class+Seat_Class_CAGR+Class_Per_Inst+Misc, data=MT)
+fit <- lm(NPV~Teach+Signup+Signup_CAGR+Ret+Buy_Class+Buy_Class_CAGR+Class_Price+Course_Cost+Users_Per_Mod+Users_Per_Mod_CAGR+Mod_Cost+Seat_Class+Seat_Class_CAGR+Class_Per_Inst+Misc, data=MT)
 boot <- boot.relimp(fit, b = 1000, type = c("first"), rank = TRUE,  diff = TRUE, rela = TRUE)
 booteval.relimp(boot) # print result
 plot(booteval.relimp(boot,sort=TRUE)) # plot result
