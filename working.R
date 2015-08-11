c2e2189fc7588f1a60011c28612e6b1d859514a5
Brad Lindaas <brad@lindaas.org>
2015-08-10 19:25:53 -0500

    Money Thingy

---
+++ working.R
@@ -0,0 +1,14 @@
+
+library(relaimpo)
+
+fit <- lm(BE~Teach+Signup+Signup_CAGR+Ret+Buy_Class+Buy_Class_CAGR+Class_Price+Course_Cost+Users_Per_Mod+Users_Per_Mod_CAGR+Mod_Cost+Seat_Class+Seat_Class_CAGR+Class_Per_Inst+Misc, data=MT)
+boot <- boot.relimp(fit, b = 1000, type = c("first"), rank = TRUE,  diff = TRUE, rela = TRUE)
+booteval.relimp(boot) # print result
+plot(booteval.relimp(boot,sort=TRUE)) # plot result
+
+
+
+
+
+names(MT)
+
